e7fbe6acd220861aadc5b018f419c48beba3e07e
anand pandit <atim4077@gmail.com>
2023-02-07 00:16:51 +0530

    added

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,14 @@
+// import "./globals.css";
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
