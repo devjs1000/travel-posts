1889d7f4ffa83e772b37ac9159319010ba84ed2b
anand pandit <atim4077@gmail.com>
2023-02-18 12:46:39 +0530

    root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import MainFooter from "@/components/MainFooter";
+import Nav from "@/components/Nav";
+import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="relative w-full min-h-[80vh]">
+        <Nav />
+        <HeroSection />
+      </div>
+      <MainFooter />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
