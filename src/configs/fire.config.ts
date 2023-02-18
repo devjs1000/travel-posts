f3cdf4e067d471a85436ec23727411b20b48fb0c
anand pandit <atim4077@gmail.com>
2023-02-18 12:47:07 +0530

    config

---
+++ src/configs/fire.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqr-tj5lMKw-9Rp9C70UCyt8jH2_QqMNI",
+    authDomain: "travel-posts.firebaseapp.com",
+    projectId: "travel-posts",
+    storageBucket: "travel-posts.appspot.com",
+    messagingSenderId: "817666491309",
+    appId: "1:817666491309:web:000725f3d09cdb96ab1724",
+    measurementId: "G-VJHR9QQTWT"
+};
+export default firebaseConfig;
